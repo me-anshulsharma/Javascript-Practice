0bea16cde1547ab592109af6f843e6685aa4b17a
Anshul Sharma <134866369+me-anshulsharma@users.noreply.github.com>
2024-10-10 06:03:43 +0000

    Memory Allocation in JS

---
+++ Part - 1 (Javascript Basics)/02_Basics.js
@@ -0,0 +1,15 @@
+// Stack (used in all Primitive datatypes - we get the copy of a variable - changes will not happen in original value)
+
+// let myName = "Anshul Sharma"
+// let myAnotherName = myName // now the myAnotherName contains the copy of myName and changes will happen in copy only
+// myAnotherName += " " + 1
+// console.log(myAnotherName);
+// console.log(myName);
+
+
+// Heap (used in all Non-Primitive datatypes - we get the referance of a variable - changes will happen directly in original value) 
+
+// let myArr = [1,2,3,4,5,6]
+// myArr[3] = "x" // now the myArr contains the referance of myArr and changes will happen in original value
+// console.log(myArr);
+
