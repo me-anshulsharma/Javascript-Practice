5171b809a4068faf4e562ce3fdacca2cb45439af
Anshul Sharma <134866369+me-anshulsharma@users.noreply.github.com>
2024-10-14 04:11:36 +0000

    DOM manipulation

---
+++ Part - 1 (Javascript Basics)/05_Basics.js
@@ -0,0 +1,12 @@
+// JS Functions
+
+// A function is a code snippet that can be called by other code or by itself, or a variable that refers to the function. 
+// When a function is called, arguments are passed to the function as input, and the function can optionally return a value. 
+// A function in JavaScript is also an object.
+
+// const sum = function addTwoNumbers(a,b){
+//     return a+b;
+// }
+
+// addTwoNumbers(5,6)
+// console.log(sum);
